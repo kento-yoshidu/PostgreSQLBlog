cd39e3840f28df3586961b284c8a424a81f7212d
kento <kento@toriwatari.bird>
2021-09-06 11:52:18 +0900

    🏃 #18 Update Heroイメージを表示

--- src/components/blogHero.js
+++ src/components/blogHero.js
@@ -1,10 +1,17 @@
 import React from "react"
 
+import { /*GatsbyImage StaticImage*/ } from "gatsby-plugin-image"
+
 import * as Styles from "../styles/blogHero.module.scss"
 
-const BlogHero = () => {
+const BlogHero = ({url}) => {
 	return (
-		<p className={Styles.blogHero}>test</p>
+		<header className={Styles.blogHero}>
+			<img	
+				src={url}
+				alt="ブログタイトル"
+			/>
+		</header>
 	)
 }
 
